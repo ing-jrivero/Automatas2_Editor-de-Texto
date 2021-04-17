54113c87677b6bc9f1bdd7a720dc5a3bf165a51f
ing_Jrivero <a17490345@itmexicali.edu.mx>
2021-04-17 16:32:22 -0700

    Editor de texto (Validar,Compilar,Correr)

---
+++ EditorTexto_Vocales/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace EditorTexto_Vocales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imprimirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compilarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.correToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.validarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.equipoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.editor = new System.Windows.Forms.RichTextBox();
+            this.rtb_errores = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.informacionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(811, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem,
+            this.imprimirToolStripMenuItem});
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.guardarComoToolStripMenuItem.Text = "Guardar Como";
+            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.guardarComoToolStripMenuItem_Click);
+            // 
+            // imprimirToolStripMenuItem
+            // 
+            this.imprimirToolStripMenuItem.Name = "imprimirToolStripMenuItem";
+            this.imprimirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.imprimirToolStripMenuItem.Text = "Imprimir";
+            this.imprimirToolStripMenuItem.Click += new System.EventHandler(this.imprimirToolStripMenuItem_Click);
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compilarToolStripMenuItem,
+            this.correToolStripMenuItem,
+            this.validarToolStripMenuItem});
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // compilarToolStripMenuItem
+            // 
+            this.compilarToolStripMenuItem.Name = "compilarToolStripMenuItem";
+            this.compilarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.compilarToolStripMenuItem.Text = "Compilar";
+            this.compilarToolStripMenuItem.Click += new System.EventHandler(this.compilarToolStripMenuItem_Click);
+            // 
+            // correToolStripMenuItem
+            // 
+            this.correToolStripMenuItem.Name = "correToolStripMenuItem";
+            this.correToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.correToolStripMenuItem.Text = "Corre";
+            this.correToolStripMenuItem.Click += new System.EventHandler(this.correToolStripMenuItem_Click);
+            // 
+            // validarToolStripMenuItem
+            // 
+            this.validarToolStripMenuItem.Name = "validarToolStripMenuItem";
+            this.validarToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.validarToolStripMenuItem.Text = "Validar";
+            this.validarToolStripMenuItem.Click += new System.EventHandler(this.validarToolStripMenuItem_Click);
+            // 
+            // informacionToolStripMenuItem
+            // 
+            this.informacionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.equipoToolStripMenuItem});
+            this.informacionToolStripMenuItem.Name = "informacionToolStripMenuItem";
+            this.informacionToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.informacionToolStripMenuItem.Text = "Informacion";
+            // 
+            // equipoToolStripMenuItem
+            // 
+            this.equipoToolStripMenuItem.Name = "equipoToolStripMenuItem";
+            this.equipoToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.equipoToolStripMenuItem.Text = "Equipo";
+            this.equipoToolStripMenuItem.Click += new System.EventHandler(this.equipoToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Errores";
+            // 
+            // editor
+            // 
+            this.editor.Location = new System.Drawing.Point(13, 28);
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(786, 242);
+            this.editor.TabIndex = 4;
+            this.editor.Text = "";
+            // 
+            // rtb_errores
+            // 
+            this.rtb_errores.Location = new System.Drawing.Point(16, 290);
+            this.rtb_errores.Name = "rtb_errores";
+            this.rtb_errores.Size = new System.Drawing.Size(783, 146);
+            this.rtb_errores.TabIndex = 5;
+            this.rtb_errores.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 448);
+            this.Controls.Add(this.rtb_errores);
+            this.Controls.Add(this.editor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imprimirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compilarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem correToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem validarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informacionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem equipoToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox editor;
+        private System.Windows.Forms.RichTextBox rtb_errores;
+    }
+}
+
---
+++ EditorTexto_Vocales/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorTexto_Vocales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string leer2;
+        private string stringToPrint;
+        string datos = "";
+        bool error = false;
+        bool est = false;
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog buscar = new OpenFileDialog();
+                if (buscar.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader leer = new StreamReader(buscar.FileName);
+                    leer2 = buscar.FileName;
+                    editor.Text = leer.ReadToEnd();
+                    leer.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error al abrir archivo");
+            }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (editor.Text.ToString().Equals(""))
+            {
+                TextWriter Escribe = new StreamWriter(leer2);
+                Escribe.WriteLine("");
+                Escribe.Close();
+            }
+            else
+            {
+
+                TextWriter Escribe = new StreamWriter(leer2);
+                Escribe.WriteLine(editor.Text);
+                Escribe.Close();
+            }
+
+        }
+
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.FileName = "*.txt";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.Filter = "txt files (*.txt) |*.txt";
+            try
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(saveFileDialog.FileName))
+                    {
+                        string txt = saveFileDialog.FileName;
+                        StreamWriter textoaguardar = File.CreateText(txt);
+                        textoaguardar.Write(editor.Text);
+                        textoaguardar.Flush();
+                        textoaguardar.Close();
+                    }
+                    else
+                    {
+                        string txt = saveFileDialog.FileName;
+                        StreamWriter textoaguardar = File.CreateText(txt);
+                        textoaguardar.Write(editor.Text);
+                        textoaguardar.Flush();
+                        textoaguardar.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al guardar archivo");
+            }
+        }
+
+        private void imprimirToolStripMenuItem_Click(object sender, EventArgs e)
+        {/*
+            printDocument1.DocumentName = leer2;
+            using (FileStream stream = new FileStream(leer2, FileMode.Open))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                stringToPrint = reader.ReadToEnd();
+            }*/
+        }
+
+        private void equipoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jesus Alexis Alicea Gonzalez \n Francisco Javier Rivero Murillo");
+
+        }
+        private void validarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Validar();
+        }
+
+        private void compilarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Compilar();
+            if (error!=true)
+            {
+                Validar();
+            }
+            
+            
+        }
+        private void correToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Compilar();
+            if (error != true)
+            {
+                Validar();
+                Ejecutar();
+            }
+        }
+
+        public void Validar()
+        {
+            datos = editor.Text;
+            char[] array = datos.ToCharArray();
+            string tem = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 'a' || array[i] == 'A')
+                {
+
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        if (array[j] != '#')
+                        {
+
+                            tem += array[j];
+                        }
+                        else
+                        {
+
+                            tem += array[j];
+                            i = j;
+                            break;
+                        }
+
+                    }
+
+                    editor.Find(tem);
+                    editor.SelectionColor = Color.Yellow;
+                    tem = "";
+
+                }
+                else if (array[i] == 'e' || array[i] == 'E')
+                {
+
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        if (array[j] != '#')
+                        {
+
+                            tem += array[j];
+                        }
+                        else
+                        {
+
+                            tem += array[j];
+                            i = j;
+                            break;
+                        }
+
+                    }
+
+                    editor.Find(tem);
+                    editor.SelectionColor = Color.Purple;
+                    tem = "";
+                }
+
+                else if (array[i] == 'i' || array[i] == 'I')
+                {
+
+
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        if (array[j] != '#')
+                        {
+
+                            tem += array[j];
+                        }
+                        else
+                        {
+
+                            tem += array[j];
+                            i = j;
+                            break;
+                        }
+
+                    }
+
+                    editor.Find(tem);
+                    editor.SelectionColor = Color.Blue;
+                    tem = "";
+
+                }
+                else if (array[i] == 'o' || array[i] == 'O')
+                {
+
+
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        if (array[j] != '#')
+                        {
+
+                            tem += array[j];
+                        }
+                        else
+                        {
+
+                            tem += array[j];
+                            i = j;
+                            break;
+                        }
+
+                    }
+
+                    editor.Find(tem);
+                    editor.SelectionColor = Color.Green;
+                    tem = "";
+
+                }
+                else if (array[i] == 'u' || array[i] == 'U')
+                {
+
+
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        if (array[j] != '#')
+                        {
+
+                            tem += array[j];
+                        }
+                        else
+                        {
+
+                            tem += array[j];
+                            i = j;
+                            break;
+                        }
+
+                    }
+
+                    editor.Find(tem);
+                    editor.SelectionColor = Color.Red;
+                    tem = "";
+
+                }
+
+            }
+            editor.AppendText(" ");
+            editor.Find(" ");
+            editor.SelectionColor = Color.Black;
+            editor.AppendText(" ");
+
+
+        }
+
+        static string Limpiar(string strIn)
+        {
+            
+            return Regex.Replace(strIn, @"[^\w\.()#]", "");
+        }
+
+        public void Compilar()
+        {
+            rtb_errores.Clear();
+            error=false;
+            datos = editor.Text;
+            string cad = Limpiar(datos);
+            Regex regex = new Regex("(i|I)nicio#.*#(e|E)nd#");
+            Match match = regex.Match(cad);
+            if (match.Success)
+            {
+           //   MessageBox.Show("correcto");
+            }
+            else
+            {
+                rtb_errores.AppendText("ERROR");
+                error = true;
+            }
+
+        }
+
+        public void Ejecutar()
+        {
+            datos = editor.Text;
+            char[] txt = datos.ToCharArray();
+            string texto = "";
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (txt[i] == ')')
+                {
+                    est = false;
+                    break;
+                }
+                else if (txt[i]=='(')
+                {
+                    est = true;
+                }
+                else if(est==true)
+                {
+                    texto += txt[i];
+                }
+            }
+            MessageBox.Show(texto);
+        }
+
+    }
+
+
+}
